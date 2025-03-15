0a8b27d773b1890589212ccee45148810aca6405
elizabetealbuquerque <114965689+elizabetealbuquerque@users.noreply.github.com>
2025-03-15 17:07:02 -0300

    Create arefaRepository.java

---
+++ arefaRepository.java
@@ -0,0 +1,7 @@
+import com.example.todolist.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
